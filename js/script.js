45cb0da71bb42f76d6a1cbbac4eea0cc63cc1da3
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-07-03 11:43:52 -0300

    Projeto concluído

--- js/script.js
+++ js/script.js
@@ -15,3 +15,37 @@ function alterarCorNavbar() {
         navbar.classList.remove('navbar-solida');
     }
 }
+
+//  ===== Carrosel =====
+
+const clientes = document.querySelectorAll(`.cliente`);
+let clienteAtual = 0;
+
+function mostrarSlide(index) {
+    clientes.forEach((cliente, i) => {
+        if (i === index) {
+            cliente.style.display = `block`;
+        } else {
+            cliente.style.display = `none`;
+        }
+    });
+}
+
+
+
+function proximo() {
+    clienteAtual = (clienteAtual + 1) % clientes.length;
+    mostrarSlide(clienteAtual);
+}
+
+function anterior() {
+    clienteAtual = (clienteAtual -1 + clientes.length) % clientes.length;
+    mostrarSlide(clienteAtual);
+}
+
+document.querySelector(`.anterior`).addEventListener(`click`, anterior);
+document.querySelector(`.proximo`).addEventListener(`click`, proximo);
+
+// Mostra o primeiro cliente ao carregar a página
+
+mostrarSlide(clienteAtual);
